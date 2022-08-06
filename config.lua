e05536f4bcc02dbbf275870a1561b30d4bea4af5
Demo <51279632+Demo4889@users.noreply.github.com>
2022-08-06 16:04:15 -0400

    Updates for QB-Core (WIP)

--- config.lua
+++ config.lua
@@ -6,15 +6,15 @@ Config = {
 
 	SellFish = { x = -3251.2, y = 991.5, z = 11.49},
 	UseFishBlip = true,
-	FishBlip = { coords = vector3(-3251.2, 991.5, 11.49)},
+	FishBlip = { coords = vector3(-3251.2, 991.5, 11.49), sprite = 356, scale = 1.0, color = 17},
 
 	SellTurtle = { x = 1338.58, y = 4359.75, z = 44.37},
 	UseTurtleBlip = true,
-	TurtleBlip = { coords = vector3(1338.58, 4359.75, 44.37)},
+	TurtleBlip = { coords = vector3(1338.58, 4359.75, 44.37), sprite = 68, scale = 1.0, color = 49},
 
 	SellShark = { x = 470.76 , y = 3552.32, z = 32.3},
 	UseSharkBlip = false,
-	SharkBlip = { coords = vector3(470.76, 3552.32, 32.3)},
+	SharkBlip = { coords = vector3(470.76, 3552.32, 32.3), sprite = 68, scale = 1.0, color = 49},
 
 	MarkerZones = {
 		{ x = -3426.7 ,y = 955.66 , z = 7.35, xs = -3426.2, ys = 942.4, zs = 1.1 },
@@ -22,5 +22,12 @@ Config = {
 		{ x = -1607.6 ,y = 5252.8 , z = 3.0, xs = -1590.2 , ys = 5278.8, zs = 1.0 },
 		{ x = 3855.0 ,y = 4463.7 , z = 1.6, xs = 3885.2 , ys =  4507.2, zs = 1.0 },
 		{ x = 1330.8  ,y = 4226.6 , z = 32.9, xs = 1334.2 , ys =  4192.4, zs = 30.0 },
-	} 
+	},
+
+	Language = {
+		fishmarket = "Fish Market",
+		turtledealer = "Turtle Dealer",
+		sharkdealer = "Shark Dealer",
+
+	}
 }
--- server.lua
+++ server.lua
@@ -1,19 +1,12 @@
+QBCore = exports['qb-core']:GetCoreObject()
 
-print("Advanced fishing started successfully")
-print("Advanced Fishing developed by Kuzaky | Discord: Kuzkay#9999")
-
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-
-ESX.RegisterUsableItem('turtlebait', function(source)
-
+QBCore.Functions.CreateUseableItem('turtlebait', function(source)
 	local _source = source
-	xPlayer = ESX.GetPlayerFromId(_source)
-	if xPlayer.getInventoryItem('fishingrod').count > 0 then
+	Player = QBCore.Functions.GetPlayer(_source)
+	if Player.getInventoryItem('fishingrod').count > 0 then
 		TriggerClientEvent('fishing:setbait', _source, "turtle")
 		
-		xPlayer.removeInventoryItem('turtlebait', 1)
+		Player.removeInventoryItem('turtlebait', 1)
 		TriggerClientEvent('fishing:message', _source, "~g~You attach the turtle bait onto your fishing rod")
 	else
 		TriggerClientEvent('fishing:message', _source, "~r~You dont have a fishing rod")
@@ -21,14 +14,13 @@ ESX.RegisterUsableItem('turtlebait', function(source)
 	
 end)
 
-ESX.RegisterUsableItem('fishbait', function(source)
-
+QBCore.Functions.CreateUseableItem('fishbait', function(source)
 	local _source = source
-	xPlayer = ESX.GetPlayerFromId(_source)
-	if xPlayer.getInventoryItem('fishingrod').count > 0 then
+	Player = QBCore.Functions.GetPlayer(_source)
+	if Player.getInventoryItem('fishingrod').count > 0 then
 		TriggerClientEvent('fishing:setbait', _source, "fish")
 		
-		xPlayer.removeInventoryItem('fishbait', 1)
+		Player.removeInventoryItem('fishbait', 1)
 		TriggerClientEvent('fishing:message', _source, "~g~You attach the fish bait onto your fishing rod")
 		
 	else
@@ -37,14 +29,13 @@ ESX.RegisterUsableItem('fishbait', function(source)
 	
 end)
 
-ESX.RegisterUsableItem('turtle', function(source)
-
+QBCore.Functions.CreateUseableItem('turtle', function(source)
 	local _source = source
-	xPlayer = ESX.GetPlayerFromId(_source)
-	if xPlayer.getInventoryItem('fishingrod').count > 0 then
+	Player = QBCore.Functions.GetPlayer(_source)
+	if Player.getInventoryItem('fishingrod').count > 0 then
 		TriggerClientEvent('fishing:setbait', _source, "shark")
 		
-		xPlayer.removeInventoryItem('turtle', 1)
+		Player.removeInventoryItem('turtle', 1)
 		TriggerClientEvent('fishing:message', _source, "~g~You attach the turtle meat onto the fishing rod")
 	else
 		TriggerClientEvent('fishing:message', _source, "~r~You dont have a fishing rod")
@@ -52,57 +43,49 @@ ESX.RegisterUsableItem('turtle', function(source)
 	
 end)
 
-ESX.RegisterUsableItem('fishingrod', function(source)
-
+QBCore.Functions.CreateUseableItem('fishingrod', function(source)
 	local _source = source
 	TriggerClientEvent('fishing:fishstart', _source)
-	
-	
-
 end)
 
-
-				
-RegisterNetEvent('fishing:catch')
-AddEventHandler('fishing:catch', function(bait)
-	
+RegisterNetEvent('fishing:catch', function(bait)
 	_source = source
 	local weight = 2
 	local rnd = math.random(1,100)
-	xPlayer = ESX.GetPlayerFromId(_source)
+	Player = QBCore.Functions.GetPlayer(_source)
 	if bait == "turtle" then
 		if rnd >= 78 then
 			if rnd >= 94 then
 				TriggerClientEvent('fishing:setbait', _source, "none")
 				TriggerClientEvent('fishing:message', _source, "~r~It was huge and it broke your fishing rod!")
 				TriggerClientEvent('fishing:break', _source)
-				xPlayer.removeInventoryItem('fishingrod', 1)
+				Player.removeInventoryItem('fishingrod', 1)
 			else
 				TriggerClientEvent('fishing:setbait', _source, "none")
-				if xPlayer.getInventoryItem('turtle').count > 4 then
+				if Player.getInventoryItem('turtle').count > 4 then
 					TriggerClientEvent('fishing:message', _source, "~r~You cant hold more turtles")
 				else
 					TriggerClientEvent('fishing:message', _source, "~g~You caught a turtle\n~r~These are endangered species and are illegal to posses")
-					xPlayer.addInventoryItem('turtle', 1)
+					Player.addInventoryItem('turtle', 1)
 				end
 			end
 		else
 			if rnd >= 75 then
-				if xPlayer.getInventoryItem('fish').count > 100 then
+				if Player.getInventoryItem('fish').count > 100 then
 					TriggerClientEvent('fishing:message', _source, "~r~You cant hold more fish")
 				else
 					weight = math.random(4,9)
 					TriggerClientEvent('fishing:message', _source, "~g~You caught a fish: ~y~~h~" .. weight .. "kg")
-					xPlayer.addInventoryItem('fish', weight)
+					Player.addInventoryItem('fish', weight)
 				end
 				
 			else
-				if xPlayer.getInventoryItem('fish').count > 100 then
+				if Player.getInventoryItem('fish').count > 100 then
 					TriggerClientEvent('fishing:message', _source, "~r~You cant hold more fish")
 				else
 					weight = math.random(2,6)
 					TriggerClientEvent('fishing:message', _source, "~g~You caught a fish: ~y~~h~" .. weight .. "kg")
-					xPlayer.addInventoryItem('fish', weight)
+					Player.addInventoryItem('fish', weight)
 				end
 			end
 		end
@@ -110,104 +93,94 @@ AddEventHandler('fishing:catch', function(bait)
 		if bait == "fish" then
 			if rnd >= 75 then
 				TriggerClientEvent('fishing:setbait', _source, "none")
-				if xPlayer.getInventoryItem('fish').count > 100 then
+				if Player.getInventoryItem('fish').count > 100 then
 					TriggerClientEvent('fishing:message', _source, "~r~You cant hold more fish")
 				else
 					weight = math.random(4,11)
 					TriggerClientEvent('fishing:message', _source, "~g~You caught a fish: ~y~~h~" .. weight .. "kg")
-					xPlayer.addInventoryItem('fish', weight)
+					Player.addInventoryItem('fish', weight)
 				end
-				
 			else
-				if xPlayer.getInventoryItem('fish').count > 100 then
+				if Player.getInventoryItem('fish').count > 100 then
 					TriggerClientEvent('fishing:message', _source, "~r~You cant hold more fish")
 				else
 					weight = math.random(1,6)
 					TriggerClientEvent('fishing:message', _source, "~g~You caught a fish: ~y~~h~" .. weight .. "kg")
-					xPlayer.addInventoryItem('fish', weight)
+					Player.addInventoryItem('fish', weight)
 				end
 			end
 		end
 		if bait == "none" then
-			
 			if rnd >= 70 then
-			TriggerClientEvent('fishing:message', _source, "~y~You are currently fishing without any equipped bait")
-				if  xPlayer.getInventoryItem('fish').count > 100 then
-						TriggerClientEvent('fishing:message', _source, "~r~You cant hold more fish")
-					else
-						weight = math.random(2,4)
-						TriggerClientEvent('fishing:message', _source, "~g~You caught a fish: ~y~~h~" .. weight .. "kg")
-						xPlayer.addInventoryItem('fish', weight)
-					end
-					
-				else
 				TriggerClientEvent('fishing:message', _source, "~y~You are currently fishing without any equipped bait")
-					if xPlayer.getInventoryItem('fish').count > 100 then
-						TriggerClientEvent('fishing:message', _source, "~r~You cant hold more fish")
-					else
-						weight = math.random(1,2)
-						TriggerClientEvent('fishing:message', _source, "~g~You caught a fish: ~y~~h~" .. weight .. "kg")
-						xPlayer.addInventoryItem('fish', weight)
-					end
+				if Player.getInventoryItem('fish').count > 100 then
+					TriggerClientEvent('fishing:message', _source, "~r~You cant hold more fish")
+				else
+					weight = math.random(2,4)
+					TriggerClientEvent('fishing:message', _source, "~g~You caught a fish: ~y~~h~" .. weight .. "kg")
+					Player.addInventoryItem('fish', weight)
 				end
+			else
+				TriggerClientEvent('fishing:message', _source, "~y~You are currently fishing without any equipped bait")
+				if Player.getInventoryItem('fish').count > 100 then
+					TriggerClientEvent('fishing:message', _source, "~r~You cant hold more fish")
+				else
+					weight = math.random(1,2)
+					TriggerClientEvent('fishing:message', _source, "~g~You caught a fish: ~y~~h~" .. weight .. "kg")
+					Player.addInventoryItem('fish', weight)
+				end
+			end
 		end
 		if bait == "shark" then
 			if rnd >= 82 then
-			
-						if rnd >= 91 then
-							TriggerClientEvent('fishing:setbait', _source, "none")
-							TriggerClientEvent('fishing:message', _source, "~r~It was huge and it broke your fishing rod!")
-							TriggerClientEvent('fishing:break', _source)
-							xPlayer.removeInventoryItem('fishingrod', 1)
-						else
-							if xPlayer.getInventoryItem('shark').count > 0  then
-									TriggerClientEvent('fishing:setbait', _source, "none")
-									TriggerClientEvent('fishing:message', _source, "~r~You cant hold more sharks")
-							else
-									TriggerClientEvent('fishing:message', _source, "~g~You caught a shark!\n~r~These are endangered species and are illegal to posses")
-									TriggerClientEvent('fishing:spawnPed', _source)
-									xPlayer.addInventoryItem('shark', 1)
-							end
-						end	
-							else
-									if xPlayer.getInventoryItem('fish').count > 100 then
-										TriggerClientEvent('fishing:message', _source, "~r~You cant hold more fish")
-									else
-										weight = math.random(4,8)
-										TriggerClientEvent('fishing:message', _source, "~g~You caught a fish: ~y~~h~" .. weight .. "kg")
-										xPlayer.addInventoryItem('fish', weight)
-									end
-								
-							end
+				if rnd >= 91 then
+					TriggerClientEvent('fishing:setbait', _source, "none")
+					TriggerClientEvent('fishing:message', _source, "~r~It was huge and it broke your fishing rod!")
+					TriggerClientEvent('fishing:break', _source)
+					Player.removeInventoryItem('fishingrod', 1)
+				else
+					if Player.getInventoryItem('shark').count > 0  then
+						TriggerClientEvent('fishing:setbait', _source, "none")
+						TriggerClientEvent('fishing:message', _source, "~r~You cant hold more sharks")
+					else
+						TriggerClientEvent('fishing:message', _source, "~g~You caught a shark!\n~r~These are endangered species and are illegal to posses")
+						TriggerClientEvent('fishing:spawnPed', _source)
+						Player.addInventoryItem('shark', 1)
+					end
+				end
+			else
+				if Player.getInventoryItem('fish').count > 100 then
+					TriggerClientEvent('fishing:message', _source, "~r~You cant hold more fish")
+				else
+					weight = math.random(4,8)
+					TriggerClientEvent('fishing:message', _source, "~g~You caught a fish: ~y~~h~" .. weight .. "kg")
+					Player.addInventoryItem('fish', weight)
+				end
 			end
-			
 		end
-	
-	
+	end
 end)
 
-RegisterServerEvent("fishing:lowmoney")
-AddEventHandler("fishing:lowmoney", function(money)
+RegisterNetEvent("fishing:lowmoney", function(money)
     local _source = source	
-	local xPlayer = ESX.GetPlayerFromId(_source)
-	xPlayer.removeMoney(money)
+	local Player = QBCore.Functions.GetPlayer(_source)
+	Player.removeMoney(money)
 end)
 
-RegisterServerEvent('fishing:startSelling')
-AddEventHandler('fishing:startSelling', function(item)
+RegisterNetEvent('fishing:startSelling', function(item)
 
 	local _source = source
 	
-	local xPlayer  = ESX.GetPlayerFromId(_source)
+	local Player  = QBCore.Functions.GetPlayer(_source)
 			if item == "fish" then
-					local FishQuantity = xPlayer.getInventoryItem('fish').count
+					local FishQuantity = Player.getInventoryItem('fish').count
 						if FishQuantity <= 4 then
 						TriggerClientEvent('esx:showNotification', source, '~r~You dont have enough~s~ fish')			
 					else   
-						xPlayer.removeInventoryItem('fish', 5)
+						Player.removeInventoryItem('fish', 5)
 						local payment = Config.FishPrice.a
 						payment = math.random(Config.FishPrice.a, Config.FishPrice.b) 
-						xPlayer.addMoney(payment)
+						Player.addMoney(payment)
 						
 						
 			end
@@ -218,29 +191,29 @@ AddEventHandler('fishing:startSelling', function(item)
 				
 			end
 			if item == "turtle" then
-				local FishQuantity = xPlayer.getInventoryItem('turtle').count
+				local FishQuantity = Player.getInventoryItem('turtle').count
 
 				if FishQuantity <= 0 then
 					TriggerClientEvent('esx:showNotification', source, '~r~You dont have enough~s~ turtles')			
 				else   
-					xPlayer.removeInventoryItem('turtle', 1)
+					Player.removeInventoryItem('turtle', 1)
 					local payment = Config.TurtlePrice.a
 					payment = math.random(Config.TurtlePrice.a, Config.TurtlePrice.b) 
-					xPlayer.addAccountMoney('black_money', payment)
+					Player.addAccountMoney('black_money', payment)
 					
 					
 				end
 			end
 			if item == "shark" then
-				local FishQuantity = xPlayer.getInventoryItem('shark').count
+				local FishQuantity = Player.getInventoryItem('shark').count
 
 				if FishQuantity <= 0 then
 					TriggerClientEvent('esx:showNotification', source, '~r~You dont have enough~s~ sharks')			
 				else   
-					xPlayer.removeInventoryItem('shark', 1)
+					Player.removeInventoryItem('shark', 1)
 					local payment = Config.SharkPrice.a
 					payment = math.random(Config.SharkPrice.a, Config.SharkPrice.b)
-					xPlayer.addAccountMoney('black_money', payment)
+					Player.addAccountMoney('black_money', payment)
 					
 					
 				end
